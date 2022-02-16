9f99399ae8fdf61dce6d20eaa055711962134030
Sigii1992 <sawlete@gmail.com>
2022-02-16 15:50:35 +0000

    Use useState to change random image and make them appear on the page

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -1,11 +1,24 @@
+import {useState} from 'react'
+import memesData from '../memesData.js'
+
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState('');
+
+  const getMemeImage= (e) => {
+    e.preventDefault();
+    const memesArray = memesData.data.memes;
+    const randomMeme = memesArray[Math.floor(Math.random() * memesArray.length)];
+    setMemeImage(prev => randomMeme.url)
+  };
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" name="top-text" placeholder="Top text here..." className="form-input" />
         <input type="text" name="bottom-text" placeholder="Bottom text here..." className="form-input" />
-        <button type="submit" className="form-button">Get a new meme image</button>
-      </form>
+        <button onClick={getMemeImage} className="form-button">Get a new meme image</button>
+      </div>
+      <img src={memeImage} className='meme-image'/>
     </main>
   )  
 }
