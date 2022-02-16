eff5a915c4d9c421fc3418bdb85c49bc8f1f9581
Sigii1992 <sawlete@gmail.com>
2022-02-16 16:57:48 +0000

    Update state to save the meme-related data as an object

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -2,13 +2,22 @@ import {useState} from 'react'
 import memesData from '../memesData.js'
 
 const Meme = () => {
-  const [memeImage, setMemeImage] = useState('');
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1g8my4.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
 
   const getMemeImage= (e) => {
     e.preventDefault();
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomMeme = memesArray[Math.floor(Math.random() * memesArray.length)];
-    setMemeImage(prev => randomMeme.url)
+    setMeme(prev => ({
+      ...prev,
+      randomImage: randomMeme.url
+      }))
   };
 
   return (
@@ -18,7 +27,7 @@ const Meme = () => {
         <input type="text" name="bottom-text" placeholder="Bottom text here..." className="form-input" />
         <button onClick={getMemeImage} className="form-button">Get a new meme image</button>
       </div>
-      <img src={memeImage} className='meme-image'/>
+      <img src={meme.randomImage} className="meme-image"/>
     </main>
   )  
 }
